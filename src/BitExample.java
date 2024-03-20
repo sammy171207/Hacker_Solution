dc600d8a2c1afff94b676396353ed76a992e2baa
Sameer Randive <96017028+sammy171207@users.noreply.github.com>
2024-03-20 09:08:34 +0530

    Add files via upload

---
+++ src/BitExample.java
@@ -0,0 +1,17 @@
+
+public class BitExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int p=9,q=10;
+		System.out.println(p|q);
+		System.out.println(p & q);
+        System.out.println(p^q);
+        System.out.println(p << 1);
+        System.out.println(p >> 2);
+        
+        System.out.println(q << 1);
+        System.out.println(q >> 2);
+	}
+
+}
